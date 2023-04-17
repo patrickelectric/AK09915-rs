4896cedc3a937375de3f3f2ca68d28369be213d8
RaulVT <raulvtrombin@gmail.com>
2023-04-17 18:15:43 -0300

    cleanup on mode,registers and functions
    add embedded_hal_mock and a test using it
    
    self_test return boolean
    
    Aplly cargo format suggestions
    
    review suggestions
    
    review suggestions : repr(u16) which limits enum instead default u32
    
    review suggestions : change u8::from to into() and enums as inputs
    
    review suggestions : use expect instead unwrap

--- examples/linux-i2c.rs
+++ examples/linux-i2c.rs
@@ -1,48 +1,47 @@
 use ak09915_rs::Ak09915;
 use ak09915_rs::Mode;
-use linux_embedded_hal::I2cdev;
 use clap::Parser;
+use linux_embedded_hal::I2cdev;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Receive the i2c device as parameter
-    #[arg(short, long, default_value_t = ("/dev/i2c-1").to_string())]
+    #[arg(short, long, default_value = "/dev/i2c-1")]
     device: String,
 }
 
 fn main() {
-
     let args = Args::parse();
     let dev = I2cdev::new(args.device).unwrap();
     let mut sensor = Ak09915::new(dev);
 
-    if sensor.self_test().unwrap(){
-        println!("Self test -  OK");     
+    if sensor.self_test().unwrap() {
+        println!("Self test -  OK");
     }
 
-    println!("Test 5 single measurement, and 5 without set single measurement(test data ready"); 
+    println!("Test 5 single measurement, and 5 without set single measurement(test data ready");
     for _n in 1..=5 {
         sensor.set_mode(Mode::Single).unwrap();
-        if sensor.is_data_ready().unwrap(){
+        if sensor.is_data_ready().unwrap() {
             let (x, y, z) = sensor.read_mag().unwrap();
             println!("Magnetometer: x={}, y={}, z={}", x, y, z);
         }
-        }
-    println!("Test 5 measurement, without set single measurement(test data ready");        
+    }
+    println!("Test 5 measurement, without set single measurement(test data ready");
     for _n in 1..=5 {
-        if sensor.is_data_ready().unwrap(){
+        if sensor.is_data_ready().unwrap() {
             let (x, y, z) = sensor.read_mag().unwrap();
             println!("Magnetometer: x={}, y={}, z={}", x, y, z);
         }
-        }
-    println!("Test 5 measurement, using continuous mode"); 
+    }
+    println!("Test 5 measurement, using continuous mode");
     sensor.set_mode(Mode::Cont200Hz).unwrap();
     for _n in 1..=5 {
-        if sensor.is_data_ready().unwrap(){
+        if sensor.is_data_ready().unwrap() {
             let (x, y, z) = sensor.read_mag().unwrap();
             println!("Magnetometer: x={}, y={}, z={}", x, y, z);
         }
         std::thread::sleep(std::time::Duration::from_micros(1000));
-        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,111 +1,100 @@
 use embedded_hal::blocking::i2c::{Write, WriteRead};
-use embedded_hal::blocking::delay::DelayUs;
 
 const AK09915_ADDRESS: u8 = 0x0C;
 
 // Register Addresses
-const AK09915_REG_WIA2: u8 = 0x01; // Device ID Register 2
-const AK09915_REG_ST1: u8 = 0x10; // Data Status Register 1
-const AK09915_REG_HXL: u8 = 0x11; // Data Register - X-axis Magnetic Data Low Byte
-const AK09915_REG_WIA1: u8 = 0x00; // Device ID Register 1
-const AK09915_REG_HXH: u8 = 0x12; // Data Register - X-axis Magnetic Data High Byte
-const AK09915_REG_HYL: u8 = 0x13; // Data Register - Y-axis Magnetic Data Low Byte
-const AK09915_REG_HYH: u8 = 0x14; // Data Register - Y-axis Magnetic Data High Byte
-const AK09915_REG_HZL: u8 = 0x15; // Data Register - Z-axis Magnetic Data Low Byte
-const AK09915_REG_HZH: u8 = 0x16; // Data Register - Z-axis Magnetic Data High Byte
-const AK09915_REG_TMPS: u8 = 0x17; // Temperature Sensor Data Register
-const AK09915_REG_ST2: u8 = 0x18; // Data Status Register 2
-const AK09915_REG_CNTL2: u8 = 0x31; // Control Register 2
-const AK09915_REG_CNTL3: u8 = 0x32; // Control Register 3
-const AK09915_REG_TS1: u8 = 0x33; // Self Test Register 1
-const AK09915_REG_TS2: u8 = 0x34; // Self Test Register 2
-const AK09915_REG_I2CDIS: u8 = 0x3A; // I2C Disable Register
+#[allow(dead_code)]
+#[repr(u16)]
+enum Register {
+    WIA1 = 0x00,   // Device ID Register 1
+    WIA2 = 0x01,   // Device ID Register 2
+    ST1 = 0x10,    // Data Status Register 1
+    HXL = 0x11,    // Data Register - X-axis Magnetic Data Low Byte
+    HXH = 0x12,    // Data Register - X-axis Magnetic Data High Byte
+    HYL = 0x13,    // Data Register - Y-axis Magnetic Data Low Byte
+    HYH = 0x14,    // Data Register - Y-axis Magnetic Data High Byte
+    HZL = 0x15,    // Data Register - Z-axis Magnetic Data Low Byte
+    HZH = 0x16,    // Data Register - Z-axis Magnetic Data High Byte
+    TMPS = 0x17,   // Temperature Sensor Data Register
+    ST2 = 0x18,    // Data Status Register 2
+    CNTL2 = 0x31,  // Control Register 2
+    CNTL3 = 0x32,  // Control Register 3
+    TS1 = 0x33,    // Self Test Register 1
+    TS2 = 0x34,    // Self Test Register 2
+    I2CDIS = 0x3A, // I2C Disable Register
+}
 
 // AK09915 Mode Settings - Corresponding to Control Register 2
-const AK09915_MODE_POWERDOWN: u8 = 0x00;
-const AK09915_MODE_SINGLE: u8 = 0x01;
-const AK09915_MODE_CONTINUOUS_10HZ: u8 = 0x02;
-const AK09915_MODE_CONTINUOUS_20HZ: u8 = 0x04;
-const AK09915_MODE_CONTINUOUS_50HZ: u8 = 0x06;
-const AK09915_MODE_CONTINUOUS_100HZ: u8 = 0x08;
-const AK09915_MODE_CONTINUOUS_200HZ: u8 = 0x0A;
-const AK09915_MODE_CONTINUOUS_1HZ: u8 = 0x0C;
-const AK09915_MODE_SELFTEST: u8 = 0x10;
+#[allow(dead_code)]
+#[repr(u16)]
+pub enum Mode {
+    PowerDown = 0x00,
+    Single = 0x01,
+    Cont10Hz = 0x02,
+    Cont20Hz = 0x04,
+    Cont50Hz = 0x06,
+    Cont100Hz = 0x08,
+    Cont200Hz = 0x0A,
+    Cont1Hz = 0x0C,
+    SelfTest = 0x10,
+}
+
+impl From<Mode> for u8 {
+    fn from(mode: Mode) -> Self {
+        mode as u8
+    }
+}
+
+impl From<Register> for u8 {
+    fn from(register: Register) -> Self {
+        register as u8
+    }
+}
 
 pub struct Ak09915<I2C> {
     pub i2c: I2C,
     pub address: u8,
 }
 
-pub enum Mode {
-    PowerDown,
-    SingleMeasurement,
-    ContMeasurement10,
-    ContMeasurement20,
-    ContMeasurement50,
-    ContMeasurement100,
-    ContMeasurement200,
-    ContMeasurement1,
-    SelfTest,
-}
-
-
 impl<I2C, E> Ak09915<I2C>
 where
     I2C: Write<Error = E> + WriteRead<Error = E>,
 {
     pub fn new(i2c: I2C) -> Self {
-        Self { i2c , address : AK09915_ADDRESS }
+        Self {
+            i2c,
+            address: AK09915_ADDRESS,
+        }
     }
 
-    fn write_register(&mut self, register: u8, value: u8) -> Result<(), E> {
-        self.i2c
-            .write(self.address, &[register, value])
+    fn write_register(&mut self, register: Register, value: u8) -> Result<(), E> {
+        self.i2c.write(self.address, &[register.into(), value])
     }
 
-    fn read_register(&mut self, register: u8) -> Result<u8, E> {
+    fn read_register(&mut self, register: Register) -> Result<u8, E> {
         let mut buffer = [0u8];
         self.i2c
-            .write_read(self.address, &[register], &mut buffer)
+            .write_read(self.address, &[register.into()], &mut buffer)
             .and(Ok(buffer[0]))
     }
 
     pub fn init(&mut self) -> Result<(), E> {
         // Soft reset device and put on continuous measurement
         self.reset()?;
-        self.set_mode(Mode::ContMeasurement50)?;
+        self.set_mode(Mode::PowerDown)?;
         Ok(())
     }
 
     pub fn reset(&mut self) -> Result<(), E> {
         // Soft reset device
-        self.write_register(AK09915_REG_CNTL3, 0x01)?;
+        self.write_register(Register::CNTL3.into(), 0x01)?;
         Ok(())
     }
 
     pub fn set_mode(&mut self, mode: Mode) -> Result<(), E> {
-        let reg = match mode {
-            Mode::PowerDown => AK09915_MODE_POWERDOWN,
-            Mode::SingleMeasurement => AK09915_MODE_SINGLE,
-            Mode::ContMeasurement10 => AK09915_MODE_CONTINUOUS_10HZ,
-            Mode::ContMeasurement20 => AK09915_MODE_CONTINUOUS_20HZ,
-            Mode::ContMeasurement50 => AK09915_MODE_CONTINUOUS_50HZ,
-            Mode::ContMeasurement100 => AK09915_MODE_CONTINUOUS_100HZ,
-            Mode::ContMeasurement200 => AK09915_MODE_CONTINUOUS_200HZ,
-            Mode::ContMeasurement1 => AK09915_MODE_CONTINUOUS_1HZ,
-            Mode::SelfTest => AK09915_MODE_SELFTEST,
-        };
-        //When user wants to change operation mode,
-        //transit to power-down mode first and then transit to other modes. After Power-down mode is set, at least 100
-        //µs (Twait) is needed before setting another mode.
-        self.write_register(AK09915_REG_CNTL2, AK09915_MODE_POWERDOWN)?;
-        
-        //not working, dirty solution
-        // let mut delay = DelayUs::
-        // delay.delay_us(100u32);
+        self.write_register(Register::CNTL2.into(), Mode::PowerDown.into())?;
         std::thread::sleep(std::time::Duration::from_micros(100));
-
-        self.write_register(AK09915_REG_CNTL2, reg)?;
+        self.write_register(Register::CNTL2.into(), mode.into())?;
         Ok(())
     }
 
@@ -124,7 +113,7 @@ where
     //     - HY[15:0] bits: -200 ≤ HY ≤ +200
     //     - HZ[15:0] bits: -800 ≤ HZ ≤ -200
 
-    pub fn self_test(&mut self) -> Result<(), E> {
+    pub fn self_test(&mut self) -> Result<bool, E> {
         self.set_mode(Mode::SelfTest)?;
 
         self.is_data_ready()?;
@@ -133,17 +122,20 @@ where
 
         // Self-test judgment
         if (hx >= -200 && hx <= 200) && (hy >= -200 && hy <= 200) && (hz >= -800 && hz <= -200) {
-            println!("Self-test passed \nMagnetometer: x={}, y={}, z={}", hx, hy, hz);
-        } else {
-            println!("Self-test failed");
+            println!(
+                "Self-test passed \nMagnetometer: x={}, y={}, z={}",
+                hx, hy, hz
+            );
+            return Ok(true);
         }
-        Ok(())
+        println!("Self-test failed");
+        return Ok(false);
     }
 
     pub fn is_data_ready(&mut self) -> Result<bool, E> {
         let mut retries = 10;
         while retries > 0 {
-            let status = self.read_register(AK09915_REG_ST1)?;
+            let status = self.read_register(Register::ST1)?;
             if (status & 0x01) != 0 {
                 return Ok(true); // Data ready
             }
@@ -155,10 +147,41 @@ where
 
     pub fn read_mag(&mut self) -> Result<(i16, i16, i16), E> {
         let mut buffer: [u8; 6] = [0u8; 6];
-        self.i2c.write_read(self.address, &[AK09915_REG_HXL], &mut buffer)?;
+        self.i2c
+            .write_read(self.address, &[Register::HXL.into()], &mut buffer)?;
         let x = i16::from_le_bytes([buffer[0], buffer[1]]);
         let y = i16::from_le_bytes([buffer[2], buffer[3]]);
         let z = i16::from_le_bytes([buffer[4], buffer[5]]);
         Ok((x, y, z))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use embedded_hal_mock::i2c::{Mock as I2cMock, Transaction as I2cTrans};
+    #[test]
+    fn set_mode_and_read_sensor() {
+        // Define expected I2C transactions for setting mode and reading sensor data
+        let expected_trans = [
+            // I2cTrans::write(0x0C, vec![0x31, 0x10]), // Set mode to continuous measurement mode 1
+            I2cTrans::write_read(0x0C, vec![0x11], vec![0x04, 0x23, 0x05, 0x24, 0x06, 0x25]), // Read sensor data
+        ];
+
+        // Create a mock I2C device and queue the expected transactions
+        let i2c_mock = I2cMock::new(&expected_trans);
+
+        // Create an AK09915 instance using the mock I2C device
+        let mut sensor = Ak09915::new(i2c_mock);
+
+        // Set the mode to continuous measurement mode 1
+        // sensor.set_mode(Mode::Cont1Hz).unwrap();
+
+        let (x, y, z) = sensor.read_mag().expect("Error reading magnetometer");
+
+        // Verify that the magnetometer data matches the expected values
+        assert_eq!(x, 0x2304);
+        assert_eq!(y, 0x2405);
+        assert_eq!(z, 0x2506);
     }
 }
